d88fac95232dd0fdc2714d196e35e9b56ca2fa89
VanshSh <vanshsharma9354@gmail.com>
2024-11-11 22:20:35 +0530

    feat: :sparkles: Title metadata object

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  // title: 'Blog', // It replaces the %s in the title in the layout
+  title: {
+    absolute: 'Blog', // It will completely replace the title in the parent
+  },
+}
+
 const Blogs = () => {
   return <h1>My Blogs</h1>
 }
